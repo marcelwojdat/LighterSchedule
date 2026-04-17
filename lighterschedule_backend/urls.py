e451f9ba531db7de4cbbe0ba72358e3a8ee6c763
MarcelWojdat <marceliwojdat@gmail.com>
2026-04-17 17:11:17 +0200

    setup: add CORS and JWT authentication

--- lighterschedule_backend/urls.py
+++ lighterschedule_backend/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from core.views import TaskTypeViewSet, WorkDayViewSet, SwapRequestViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 
@@ -12,4 +13,6 @@ router.register(r'swaps', SwapRequestViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
