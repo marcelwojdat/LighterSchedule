4b71ca78879087b28f0d7d1770c28e4cbbcb1c25
MarcelWojdat <marceliwojdat@gmail.com>
2026-04-17 17:30:58 +0200

    feat: implement user registration and JWT authentication

--- core/views.py
+++ core/views.py
@@ -1,8 +1,33 @@
 from rest_framework import viewsets, permissions
 from django.db import models
 from django.db.models import Q
-from .models import TaskType, WorkDay, SwapRequest
+from .models import TaskType, WorkDay, SwapRequest, EmployeeProfile
 from .serializers import TaskTypeSerializer, WorkDaySerializer, SwapRequestSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+
+def register_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    
+    if not username or not password:
+        return Response({'error': 'Podaj login i hasło'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if User.objects.filter(username=username).exists():
+        return Response({'error': 'Użytkownik już istnieje'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_user(username=username, password=password)
+    EmployeeProfile.objects.create(user=user) # Tworzymy profil automatycznie
+    
+    return Response({'message': 'Zarejestrowano pomyślnie'}, status=status.HTTP_201_CREATED)
+
 
 class TaskTypeViewSet(viewsets.ModelViewSet):
     queryset = TaskType.objects.all()
--- lighterschedule_backend/urls.py
+++ lighterschedule_backend/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
-from core.views import TaskTypeViewSet, WorkDayViewSet, SwapRequestViewSet
+from core.views import TaskTypeViewSet, WorkDayViewSet, SwapRequestViewSet, register_user
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
@@ -12,6 +12,7 @@ router.register(r'swaps', SwapRequestViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/register/', register_user, name='register'),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
